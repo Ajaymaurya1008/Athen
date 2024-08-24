ff79d652601ded25e0065694e28db185b37c294e
Ajaymaurya1008 <ajaykvmaurya@gmail.com>
2024-08-24 20:54:29 +0530

    add fonts and images

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,8 +1,25 @@
 import { Stack } from "expo-router";
+import { useFonts } from "expo-font";
 
 export default function RootLayout() {
+  const [fontsLoaded] = useFonts({
+    "Inter-100": require("@/assets/fonts/Inter-100.ttf"),
+    "Inter-200": require("@/assets/fonts/Inter-200.ttf"),
+    "Inter-300": require("@/assets/fonts/Inter-300.ttf"),
+    "Inter-400": require("@/assets/fonts/Inter-400.ttf"),
+    "Inter-500": require("@/assets/fonts/Inter-500.ttf"),
+    "Inter-600": require("@/assets/fonts/Inter-600.ttf"),
+    "Inter-700": require("@/assets/fonts/Inter-700.ttf"),
+    "Inter-800": require("@/assets/fonts/Inter-800.ttf"),
+    "Poppins-bold": require("@/assets/fonts/Poppins-Bold.ttf"),
+  });
+
+  if (!fontsLoaded) {
+    return null;
+  }
+
   return (
-    <Stack>
+    <Stack screenOptions={{ headerShown: false }}>
       <Stack.Screen name="index" />
     </Stack>
   );
--- app/index.tsx
+++ app/index.tsx
@@ -1,15 +1,21 @@
-import { Text, View } from "react-native";
+import { StatusBar, Text, View, StyleSheet } from "react-native";
+import { SafeAreaView } from "react-native-safe-area-context";
 
 export default function Index() {
   return (
-    <View
-      style={{
-        flex: 1,
-        justifyContent: "center",
-        alignItems: "center",
-      }}
-    >
-      <Text>Edit app/index.tsx to edit this screen.</Text>
-    </View>
+    <SafeAreaView className="flex-1 items-center justify-center bg-white">
+      <StatusBar barStyle={"dark-content"} />
+      <View className="flex-1 items-center justify-center ">
+        <Text style={styles.font} className="text-[40px] ">
+          Hello World!
+        </Text>
+      </View>
+    </SafeAreaView>
   );
 }
+
+const styles = StyleSheet.create({
+  font: {
+    fontFamily: "Inter-800",
+  },
+});
